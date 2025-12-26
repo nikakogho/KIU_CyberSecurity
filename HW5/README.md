5ca7906b5396b2b1ec27754282ecc681f46538ad
nikakogho <nikakoghuashvili@gmail.com>
2025-12-26 20:29:31 +0400

    HW5 start

---
+++ HW5/README.md
@@ -0,0 +1,14 @@
+HW 5 of Cyber Security Course at KIU
+
+We must 
+0. Host app on `localhost:80`
+1. Block incoming HTTP traffic to `localhost:80`
+2. Test that it is blocked
+3. Unblock
+
+## 0. Hosting App On localhost:80
+We write a simple python server script (visible at `simple_server.py` and run it to host on `localhost:80`)
+
+![Hosted](images/hosted.png)
+
+## 
---
+++ HW5/simple_server.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"""
+        <html>
+            <head><title>Port 80 Test</title></head>
+            <body>
+                <h1>Hello from Python on port 80!</h1>
+                <p>If you see this, port 80 is open.</p>
+            </body>
+        </html>
+        """)
+
+server_address = ("", 80)
+httpd = HTTPServer(server_address, SimpleHandler)
+
+print("Serving HTTP on port 80...")
+httpd.serve_forever()
